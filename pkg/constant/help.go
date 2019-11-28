e99c84978894824618e2862d63d30847f7c08a47
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-11-28 23:53:14 +0900

    feat: improve the help message

--- pkg/constant/help.go
+++ pkg/constant/help.go
@@ -1,5 +1,21 @@
 package constant
 
 const (
-	Help = "ddtime - post the command execution time as time-series data to DataDog"
+	Help = `dd-time - post the command execution time as time-series data to DataDog
+
+https://github.com/suzuki-shunsuke/dd-time
+
+USAGE:
+  dd-time [options] -- command
+
+ENVIRONMENT VARIABLE
+
+  DATADOG_APK_KEY (required) - DataDog APK Key
+
+OPTIONS:
+  --help, -h                     show help
+  --version, -v                  print the version
+  --metric-name value, -m value  (default: "command-execution-time") The name of the time series
+  --host value                   The name of the host that produced the metric
+  --tag value, -t value          A tag associated with the metric`
 )
