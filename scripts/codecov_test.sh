713ba148759aa086286b7c1665abde2089f13b76
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2020-01-03 18:55:59 +0900

    ci: use shellcheck and shfmt

--- scripts/codecov_test.sh
+++ scripts/codecov_test.sh
@@ -2,17 +2,19 @@
 # https://github.com/codecov/example-go#caveat-multiple-files
 
 ee() {
-  echo "+ $@"
+  echo "+ $*"
   eval "$@"
 }
 
-echo "" > coverage.txt
+cd "$(dirname "$0")/.." || exit 1
 
-for d in `go list ./...`; do
-  echo $d
-  ee go test -race -coverprofile=profile.out -covermode=atomic $d || exit 1
+echo "" > coverage.txt || exit 1
+
+for d in $(go list ./...); do
+  echo "$d" || exit 1
+  ee go test -race -coverprofile=profile.out -covermode=atomic "$d" || exit 1
   if [ -f profile.out ]; then
-    cat profile.out >> coverage.txt
-    rm profile.out
+    cat profile.out >> coverage.txt || exit 1
+    rm profile.out || exit 1
   fi
 done
